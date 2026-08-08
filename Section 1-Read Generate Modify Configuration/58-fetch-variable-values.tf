01ad53c00382a5a19b2a8f9df48671750af03eff
vimalkaur1981 <kaurvimal81@gmail.com>
2026-08-08 23:41:49 +0800

    data types-fetch values

---
+++ Section 1-Read Generate Modify Configuration/58-fetch-variable-values.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "myec2" {
+   ami = "ami-05a67986ec8a2dcf8"
+   instance_type = var.list[1]
+}
+
+variable "list" {
+  type = list
+  default = ["m5.large","m5.xlarge","t2.medium"]
+}
+
+variable "types" {
+  type = map
+  default = {
+    us-east-1 = "t2.micro"
+    us-west-2 = "t2.nano"
+    ap-south-1 = "t2.small"
+  }
+}
