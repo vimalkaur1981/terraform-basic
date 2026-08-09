8e637412029ab5fc2af19a9b6c4a6a938c7e8729
vimalkaur1981 <kaurvimal81@gmail.com>
2026-08-09 13:07:32 +0800

    data source filter

--- Section 1-Read Generate Modify Configuration/66-data-source.tf
+++ Section 1-Read Generate Modify Configuration/66-data-source.tf
@@ -1,3 +1,19 @@
+provider "aws" {
+    region = ap-southeast-1
+
+}
 data "local_file" "foo" {
     filename = "${path.module}/66-demofile.txt"  
+}
+
+output "data" {
+  value = data.local_file.foo.content
+}
+
+data "aws_instance" "ec2_instances" {
+    filter {
+        name = "tag.Team"
+        values = ["Production"]
+    }
+  
 }
